636f0946c4ddcce055399734960b207c75122439
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-09-17 11:38:08 +0100

    udpate

--- src/main/java/com/example/restapi/controller/Controller.java
+++ src/main/java/com/example/restapi/controller/Controller.java
@@ -1,4 +1,16 @@
 package com.example.restapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/new-feature")
 public class Controller {
+
+
+    @GetMapping("")
+    public String get(){
+        return "Hello this is the new feature";
+    }
 }
